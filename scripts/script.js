376633e07a579a4b8bd534c5423f01f43accc285
Praveensk06 <113981543+Praveensk06@users.noreply.github.com>
2023-02-24 17:12:37 +0530

    change variables and change title in html and change body background color

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,49 @@
 // get elements
-const btnEl=document.querySelectorAll('.btn');
+const btnEl = document.querySelectorAll(".btn");
 const boxEl = document.querySelectorAll(".box");
-const searchEl=document.getElementById('search');
-
+const searchEl = document.getElementById("search");
 
 // search product
-searchEl.addEventListener('keyup',(e)=>{
-  const searchText=(e.target.value.toLowerCase().trim());
+searchEl.addEventListener("keyup", (e) => {
+  const searchText = e.target.value.toLowerCase().trim();
 
-boxEl.forEach((box)=>{
-  const data=box.dataset.item;
-  if(data.includes(searchText)){
-    box.style.display='block';
-  }else{ 
-    box.style.display = "none";
-  }
-});
+  boxEl.forEach((box) => {
+    const data = box.dataset.item;
+    if (data.includes(searchText)) {
+      box.style.display = "block";
+    } else {
+      box.style.display = "none";
+    }
+  });
   btnEl[0].classList.add("btn-clicked");
 });
 
 // select all btn
-btnEl.forEach((button)=>{
-  button.addEventListener('click',(e)=>{
+btnEl.forEach((button) => {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
     setActiveBtn(e);
-    const btnfi
-    lter=e.target.dataset.filter;
+    const btnfilter = e.target.dataset.filter;
 
-    boxEl.forEach((box)=>{
-      if(btnfilter=='all'){
-        box.style.display='block';
-
-      }else{
-        const boxFilter=box.dataset.item;
-        if(btnfilter==boxFilter){
-          box.style.display='block';
-        }else{
-          box.style.display='none';
+    boxEl.forEach((box) => {
+      if (btnfilter == "all") {
+        box.style.display = "block";
+      } else {
+        const boxFilter = box.dataset.item;
+        if (btnfilter == boxFilter) {
+          box.style.display = "block";
+        } else {
+          box.style.display = "none";
         }
-
       }
-    })
+    });
   });
 });
 
 // function active btn
-function setActiveBtn(e){
+function setActiveBtn(e) {
   btnEl.forEach((button) => {
-    button.classList.remove('btn-clicked');
+    button.classList.remove("btn-clicked");
   });
-e.target.classList.add('btn-clicked')
-
+  e.target.classList.add("btn-clicked");
 }
